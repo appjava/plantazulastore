d30649fcae49e0396241e16933fc95e2c025dc99
javaair <appjavadev2019@gmail.com>
2024-08-28 18:10:47 -0500

    update files

--- public/src/show.js
+++ public/src/show.js
@@ -49,48 +49,40 @@ let showProduct = () => {
       let search = standItemsData.find((x) => x.id === id) || [];
       let search2 = basket.find((x) => x.id === id) || [];
       return `
-      
-      <!--<div class="showcase">
-          <img class="" src=${search.img === undefined ? ["images/logos/logoClau.png"] : search.img} >
-          <img class="" src=${search.img1 === undefined ? ["images/logos/logoClau.png"] : search.img1} >
-          <img class="" src=${search.img2 === undefined ? ["images/logos/logoClau.png"] : search.img2} >
-          <img class="" src=${search.img3 === undefined ? ["images/logos/logoClau.png"] : search.img3} >
-      </div>-->
+
       <div class="showcase">
-      <div class="pic-ctn">
-          <img src=${search.img === undefined ? ["images/logos/logoClau.png"] : search.img} alt="" class="pic">
-          <img src=${search.img1 === undefined ? ["images/logos/logoClau.png"] : search.img1} alt="" class="pic">
-          <img src=${search.img2 === undefined ? ["images/logos/logoClau.png"] : search.img2} alt="" class="pic">
-          <img src=${search.img3 === undefined ? ["images/logos/logoClau.png"] : search.img3} alt="" class="pic">
-      </div>
+        <div class="pic-ctn">
+            <img src=${search.img === undefined ? ["images/logos/logoPlantaZula.png"] : search.img} alt="" class="pic">
+            <img src=${search.img1 === undefined ? ["images/logos/logoPlantaZula.png"] : search.img1} alt="" class="pic">
+            <img src=${search.img2 === undefined ? ["images/logos/logoPlantaZula.png"] : search.img2} alt="" class="pic">
+            <img src=${search.img3 === undefined ? ["images/logos/logoPlantaZula.png"] : search.img3} alt="" class="pic">
+        </div>
       </div>
       <div>
-      <div class="detail">
-        <br>
-        <h3 class="detail">${search.desc}</h3>
-        <br>
-      </div>
-      <!--
-      <br>
-      <p class="desc-item-show">${search.desc1 === undefined ? "" : search.desc1}</p>
-      <br>
-      -->
-      <div id=product-id-${search.id} class="item-show">
-        <br>
-        <div class="price-quantity">
-          <div class="buttons-show">
-            <h3 class="price-item-show" onclick="increment(${search.id})">$ ${search.price} COP</h3>
-            <i onclick="increment(${search.id})" class="bi bi-cart-plus-fill"></i>
-            <div id=${search.id} class="quantity-item-show">
-              ${search2.item === undefined ? "" : search2.item}
+        <div class="detail">
+          <br>
+          <h3 class="detail">${search.desc}</h3>
+          <br>
+        </div>
+      
+        <div id=product-id-${search.id} class="item-show">
+          <br>
+          <div class="price-quantity">
+            <div class="buttons-show">
+              <h3 class="price-item-show" onclick="increment(${search.id})">$ ${search.price} COP</h3>
+              <i onclick="increment(${search.id})" class="bi bi-cart-plus-fill"></i>
+              <div id=${search.id} class="quantity-item-show">
+                ${search2.item === undefined ? "" : search2.item}
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
+
       <a href="index.html">
       <button class="HomeBtn">Volver a Estante</button>
       </a>
+      
     `;}).join(""));
 };
 
